edbb9092e611156876bbe42fb8fe4c9e105b9a4f
schristinelee <christinelee1527@gmail.com>
2022-11-15 11:56:10 -0800

    makes request with hidden api key

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   def index
-    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=APIKEY")
+    key = Rails.application.credentials.news_api[:api_key]
+
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=#{key}")
     render json: response.parse(:json)
   end
 end
