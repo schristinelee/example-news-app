af8213b7bd0ddbe049afe8b9021a7fac601583a4
schristinelee <christinelee1527@gmail.com>
2022-11-15 11:37:16 -0800

    adds index index action in the articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,6 @@
 class ArticlesController < ApplicationController
+  def index
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=APIKEY")
+    render json: response.parse(:json)
+  end
 end
